bbc98515ebc5c50f9ca31d6d283712682b40fe89
max80057 <max80057@gmail.com>
2021-12-08 10:51:32 +0800

    Update OptionalTest.java

--- JAVA 8/01_Optional/OptionalTest.java
+++ JAVA 8/01_Optional/OptionalTest.java
@@ -12,11 +12,14 @@ public class OptionalTest {
 
         String nickName = nickNames.get(name);
 
+        //Optional.value預設為null
+        //用ofNullable方法，不為null時直接丟值到Optional.value，否則就是null
         return Optional.ofNullable(nickName);
     }
 
     public static void main(String[] args) {
         Optional<String> nickNameOptionalTest = getNickName("Duke");
+        //用orElse方法，直接回傳值，若為null的話回傳傳入值
         System.out.println(nickNameOptionalTest.orElse("Openhome Reader"));
 
         nickNameOptionalTest = getNickName("Justin");
